675e8fb9e9b52603f37aa392ea666712c1040037
Elliott Wilkinson <ellisocks@gmail.com>
2024-02-22 11:35:43 -0500

    getting through components

---
+++ Client/src/components/ArtCards.vue
@@ -0,0 +1,14 @@
+<template>
+  <section id="art-card"></section>
+</template>
+
+<script>
+import GalleryService from '../services/GalleryService';
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ Client/src/components/ArtDetails.vue
@@ -0,0 +1,30 @@
+<template>
+  <section id="art-details">
+      <article class="art-details">
+          <div class="art-image">
+              <img :src="artPiece.imageName">
+          </div>
+          <div class="art-caption">
+              {{artPiece.caption}}
+          </div>
+          <div class="art-description">
+              {{artPieced.description}}
+          </div>
+      </article>
+  </section>
+</template>
+
+<script>
+export default {
+
+    props: ['artPiece'], 
+
+    computed: { 
+
+    }
+}
+</script>
+
+<style>
+
+</style>
